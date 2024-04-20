40a2e5e9bc9e315a0c9aece734f779a1805fa2a9
Pablo Chu-Bin <chubinco@gmail.com>
2024-04-20 17:57:52 +0300

    correct rexec

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import pytest
+
+
+@pytest.fixture()
+def json_schema():
+    schema = {
+        "type": "object",
+        "properties": {
+            "data": {
+                "type": "object",
+                "properties": {
+                    "names": {
+                        "type": "array",
+                        "items": {"type": "string"}
+                    },
+                    "tensor": {
+                        "type": "object",
+                        "properties": {
+                            "shape": {
+                                "type": "array",
+                                "items": {"type": "integer", "minimum": 1, "maximum": 10}
+                            },
+                            "values": {
+                                "type": "array",
+                                "items": [
+                                    {"type": "string", "pattern": "^[0-9]{1,6}$"},
+                                    {"type": "string", "maxLength": 100},
+                                    {"type": "integer", "minimum": 1},
+                                    {"type": "string", "maxLength": 100}
+                                ]
+                            }
+                        },
+                        "required": ["shape", "values"],
+                        "additionalProperties": False
+                    }
+                },
+                "required": ["names", "tensor"],
+                "additionalProperties": False
+            }
+        }
+    }
+    return schema
+
---
+++ test_json_schema.py
@@ -0,0 +1,33 @@
+import pytest
+from jsonschema import Draft201909Validator
+
+
+@pytest.mark.parametrize("response", [
+    {
+        "data": {
+            "names": ["messageId", "phraseText", "topK", "cus"],
+            "tensor": {"shape": [1, 4],
+                       "values": [
+                           "12345",
+                           "Подскажите как оформить дебетовую карту альфа банка?",
+                           1,
+                           "test_cus_8"]
+                       }
+        }
+    },
+    {
+        "data": {
+            "names": ["messageId", "phraseText", "topK", "cus"],
+            "tensor": {"shape": [1, 5],
+                       "values": [
+                           "19",
+                           "Подскажите как оформить кредитную карту и ипотеку  альфа банка? ",
+                           11,
+                           "test_cus_11",
+                           "summ"]
+                       }
+        }
+    }
+])
+def test_json_schema_validator(json_schema, response):
+    Draft201909Validator(json_schema).validate(response)
